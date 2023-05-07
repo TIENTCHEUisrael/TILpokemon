826d8e121059473b5d9496c986c5260a1562ce97
TIENTCHEUisrael <israel01tientcheu@gmail.com>
2023-05-07 13:01:47 +0200

    Initialisation a angular et utilisation de Oninit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'mypokemonApp';
+export class AppComponent implements OnInit{
+ 
+  title = 'TILpokemonApp';
+  pokemons=['Bulbizarre','Salamèche','Carapuce'];
+  ngOnInit(): void {
+   console.table(this.pokemons);
+  }
 }
+ 
