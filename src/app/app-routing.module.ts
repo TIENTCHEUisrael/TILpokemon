df72fd36256ea75830c1de3bbf9fc8d63951f27b
TIENTCHEUisrael <israel01tientcheu@gmail.com>
2023-05-21 20:46:37 +0200

    configuration du module pokemon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
-import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [
-  {path:"pokemons",component:ListPokemonComponent},
-  {path:"pokemon/:id",component:DetailPokemonComponent},  
+const routes: Routes = [ 
   {path:"",redirectTo:'pokemons',pathMatch:"full"},
   {path:'**',component:PageNotFoundComponent}//Intercepte toute les routes et les renvoye dans page not found
 ];
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { POKEMONS } from './mock-pokemon-list';
-import { Pokemon } from './pokemon';
-import { BorderCardDirective } from './border-card.directive';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
