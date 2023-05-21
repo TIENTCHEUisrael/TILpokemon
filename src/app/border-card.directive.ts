037896b085d02502a965de00f22c2b8bc8e11275
TIENTCHEUisrael <israel01tientcheu@gmail.com>
2023-05-21 17:42:35 +0200

    conclusion directives

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,21 +1,27 @@
-import { Directive, ElementRef,HostListener } from '@angular/core';
+import { Directive, ElementRef,HostListener,Input } from '@angular/core';
 //ElementRef pour representer une reference vers un card
 //HostListener pour detecter les evenements gerer par l'utilisateur
+//Input va servir ici a parametrer les elements a changer pour eviter l'effet statique
 @Directive({
   selector: '[pkmnBorderCard]'
 })
 export class BorderCardDirective {
+  private initalColor:string='#f5f5f5';
+  private defaultColor:string='#009688';
+  private defaultHeight:number=180;
   constructor( private el:ElementRef) {
-    this.setHeight(180); 
-    this.setBorder('#f5f5f5');
+    this.setHeight(this.defaultHeight); 
+    this.setBorder(this.initalColor);
   }
 
+  @Input('pkmnBorderCard') borderColor:string;
+
   @HostListener('mouseenter') onMouseEnter(){
-    this.setBorder('#009688');
+    this.setBorder(this.borderColor || this.defaultColor);
   }
 
   @HostListener('mouseleave') onMouseLeave(){
-    this.setBorder('#f5f5f5');
+    this.setBorder(this.initalColor);
   }
 
   private setHeight(height:number){
