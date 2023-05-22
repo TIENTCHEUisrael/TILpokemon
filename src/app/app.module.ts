79ecd00762c175c2537a436456e6e9530ad00fa9
TIENTCHEUisrael <israel01tientcheu@gmail.com>
2023-05-22 11:51:01 +0200

    installer angular-in-memory-web-api pour simuler une api et simuler celui ci

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -15,9 +15,9 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     PokemonModule,
-    AppRoutingModule
-    
+    AppRoutingModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
